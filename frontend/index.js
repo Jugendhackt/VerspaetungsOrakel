cfbb45e27767c8ee97428e1755caf86522e3daeb
Erik <83910726+jserik@users.noreply.github.com>
2023-07-15 15:19:21 +0200

    jfjf

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,11 @@
-let train = document.getElementById("train-id").value;
-let station = document.getElementById("station-id").value;
+let train = document.getElementById("train-info").value[0];
+let station = document.getElementById("train-info").value[1];
+
 fetch("/api/submit", {
   method: "POST",
   body: JSON.stringify({
-    train: name,
-    station: name,
+    train: train,
+    station: station,
   }),
   headers: {
     "Content-type": "application/json; charset=UTF-8"
