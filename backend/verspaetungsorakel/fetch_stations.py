fce55c4459b8f5f32d7bfb81467e1cfeb5e4f99a
code1337 <115311343+c0d3-1337@users.noreply.github.com>
2023-07-15 20:09:37 +0200

    Fetch timetables (#10)
    * Timetable API implementation
    
    * l

--- backend/verspaetungsorakel/fetch_stations.py
+++ backend/verspaetungsorakel/fetch_stations.py
@@ -34,10 +34,19 @@ def get_stations(query: str) -> str:
 
 def insert_stations_to_db() -> None:
     stations = get_stations("*")["stations"]["station"]
+    db_stations = []
 
     for station in stations:
-        # print(s)
-        model.Station.replace(name=station["@name"], number=station["@eva"], ds100=station["@ds100"]).execute()
+        # print(station)
+        # model.Station.get_or_create(name=station["@name"], number=station["@eva"], ds100=station["@ds100"])
+        db_stations.append({
+            "name": station["@name"],
+            "number": station["@eva"],
+            "ds100": station["@ds100"]})
+    
+    with model.db.atomic():
+        # TODO: Don't ignore conflicts, update row instead
+        model.Station.insert_many(db_stations).on_conflict_ignore().execute()
 
 def main():
     model.connect()
