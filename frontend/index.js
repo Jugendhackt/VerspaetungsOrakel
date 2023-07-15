5897117e1fdf748e95659e097bef65d313cd1052
Erik <83910726+jserik@users.noreply.github.com>
2023-07-15 16:14:35 +0200

    new fetch

--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,24 @@
-let train = document.getElementById("train-info").value[0];
-let station = document.getElementById("train-info").value[1];
+function postRequest() {
+  let train = document.getElementById("train-id").value
+  let station = document.getElementById("station-id").value;
 
-fetch("/api/submit", {
+  fetch("https://172.16.7.194:5000/api/submit", {
   method: "POST",
   body: JSON.stringify({
-    train: train,
-    station: station,
+      train: train,
+      station: station,
   }),
   headers: {
-    "Content-type": "application/json; charset=UTF-8"
+      "Content-type": "application/json; charset=UTF-8",
+      'Access-Control-Allow-Origin':'*'
+
   }
-})
+  })
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => console.log("hi"));
+}
+
+function displayResult() {
+  // let resultfield = document.getElementById("result");
+  document.getElementById("result").innerHTML = json;
+}
