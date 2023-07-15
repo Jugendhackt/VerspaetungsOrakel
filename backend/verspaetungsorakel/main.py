3c398270ff2ce1926f67e608a8a53d698a13f646
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-07-15 15:22:34 +0200

    webserver (#1)
    * einfacher webserver + submit api
    
    * submit gefixt

--- backend/verspaetungsorakel/main.py
+++ backend/verspaetungsorakel/main.py
@@ -1,3 +1,35 @@
+from flask import Flask, request, jsonify
+from peewee import fn
+
 import verspaetungsorakel.model as model
 
-model.connect()
+app = Flask(__name__)
+
+
+@app.route("/api/submit")
+def submit():
+    train = request.args.get("train")
+    station = request.args.get("station")
+
+    stops = model.Stop.select().where(
+        model.Stop.station.number == station,
+        model.Stop.trip.train.number == train
+    )
+
+    total_delay = 0
+    for stop in stops:
+        total_delay += stop.arrival_delay
+
+    average_delay = total_delay / len(stops)
+
+    return jsonify({"average_delay": average_delay}), 200
+
+
+# @app.route("/api/stations")
+def main():
+    model.connect()
+    app.run(host="0.0.0.0")
+
+
+if __name__ == "__main__":
+    main()
